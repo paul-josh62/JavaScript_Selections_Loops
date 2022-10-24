de5d5ddd8fd38bd94ded724875ab404465ff4c07
Joshua Paul <paul.josh62@gmail.com>
2022-10-24 14:04:52 -0500

    inProgress

--- app.js
+++ app.js
@@ -3,5 +3,11 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+for (let i = 1; i <= 100; i++) {
+  if (i % 2) {
+    console.log(i);
+  }
+}
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
